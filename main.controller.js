a1f9ea1d2e4f981ae795a73cb90656c54804b168
Kerley Dantas <kerleysol@gmail.com>
2017-10-26 11:16:25 -0300

    MVP

---
+++ main.controller.js
@@ -0,0 +1,41 @@
+angular.module("TestProcess", ['ui.sortable']).controller("MainController", function($scope, $http, $interval){
+  var baseURL = "http://localhost:3000/"
+
+  $scope.pullRequests = [];
+  $scope.options = [true, false]
+  $scope.getData = _getData
+  $scope.changeStatus = _changeStatus
+
+  _getData();
+
+  $interval(_getData, 1000 * 60)
+
+  $scope.sortableOptions = {
+    stop: function(e, ui) {
+      for (var index in $scope.pullRequests) {
+       $scope.pullRequests[index].priority = $scope.pullRequests.length - index;
+      }
+      $http.post(baseURL+"changedPriority", $scope.pullRequests).then(function(res){
+      }, function(err){
+        console.log(err)
+      })
+    }
+  };
+
+  function _changeStatus(update){
+    $http.put(baseURL+"pullRequest/"+update.id,update).then(function(res){
+    }, function(err){
+      console.log(err)
+    })
+  }
+
+  function _getData(){
+    $http.get(baseURL+"openedPullRequests",{ headers: { 'Cache-Control' : 'no-cache' } }).then(function(res){
+      $scope.pullRequests = res.data
+    }, function(err){
+      console.log(err)
+    })
+  
+  }
+
+});
